64b5e786523adae524910d5cd80c460e2eee6140
Микола Лук'яник <Lukyanyk@rivne.itstep.org>
2019-12-05 19:07:49 +0200

    add del

--- src/components/ConstacList/ContactList.js
+++ src/components/ConstacList/ContactList.js
@@ -1,26 +1,32 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import "./contactlist.css";
 import ContactItem from "./ContactItem/ContactItem";
 
-const ContactList = ({ContactList}) => {
-        const listItem = ContactList.map((item) =>{
-            
-            return <ContactItem avatar={item.avatar} name={item.name} description={item.description} gender={item.gender} iconfacebook={item.iconfacebook} icontwitter={item.icontwitter} iconlinkedin= {item.iconlinkedin} iconskype={item.iconskype} iconemail={item.iconemail}
-            ></ContactItem>
-        })
+const ContactList = ({ ContactList, onDelete }) => {
+  const listItem = ContactList.map(item => {
     return (
-        <Fragment>
-            <h2 className="col-7 d-flex justify-content-center">ContactList</h2>
-            {listItem}
-            
-            
-
-
-
-
-        </Fragment>
-       
+      <ContactItem
+        key={item.id}
+        avatar={item.avatar}
+        name={item.name}
+        description={item.description}
+        gender={item.gender}
+        iconfacebook={item.iconfacebook}
+        icontwitter={item.icontwitter}
+        iconlinkedin={item.iconlinkedin}
+        iconskype={item.iconskype}
+        iconemail={item.iconemail}
+        favorite={item.favorite}
+        onDelete={() => onDelete(item.id)}
+      ></ContactItem>
     );
-}
+  });
+  return (
+    <Fragment>
+      <h2 className="col-7 d-flex justify-content-center">ContactList</h2>
+      {listItem}
+    </Fragment>
+  );
+};
 
 export default ContactList;
--- src/index.js
+++ src/index.js
@@ -8,69 +8,105 @@ import './index.css';
 
 
     class App extends React.Component{
-      List = [
-        {
-            name: "Johnny Depp",
-            description: "Actor",
-            avatar: 20,
-            gender: "men",
-            iconfacebook: "https://ru-ru.facebook.com/JohnChristopherOfficial/",
-            icontwitter: "https://ru-ru.facebook.com/JohnChristopherOfficial/",
-            iconlinkedin: "https://www.linkedin.com/",
-            iconskype: "https://www.skype.com/uk/",
-            iconemail:"https://www.google.com/intl/uk/gmail/about/"
-        },
-        {
-            name: "Arnold Schwarzenegger",
-            description: "Actor",
-            avatar: 35,
-            gender: "men",
-            iconfacebook:"https://uk-ua.facebook.com/arnold",
-            icontwitter: "https://ru-ru.facebook.com/JohnChristopherOfficial/",
-            iconlinkedin: "https://www.linkedin.com/",
-            iconskype: "https://www.skype.com/uk/",
-            iconemail:"https://www.google.com/intl/uk/gmail/about/"
-        },
-        {
-            name: "Sylvester Stallone",
-            description: "Actor",
-            avatar: 10,
-            gender: "men",
-            iconfacebook:"https://www.facebook.com/SylvesterStallone/",
-            icontwitter: "https://ru-ru.facebook.com/JohnChristopherOfficial/",
-            iconlinkedin: "https://www.linkedin.com/",
-            iconskype: "https://www.skype.com/uk/",
-            iconemail:"https://www.google.com/intl/uk/gmail/about/"
-        },
-        {
-            name: "Milla Jovovich",
-            description: "Actor",
-            avatar: 30,
-            gender: "women",
-            iconfacebook:"https://www.facebook.com/MillaJovovich/",
-            icontwitter: "https://twitter.com/millajovovich",
-            iconlinkedin: "https://www.linkedin.com/",
-            iconskype: "https://www.skype.com/uk/",
-            iconemail:"https://www.google.com/intl/uk/gmail/about/"
-        },
-        {
-            name: "Jon Bon Jovi;",
-            description: "Singer",
-            avatar: 50,
-            gender:"men",
-            iconfacebook:"https://www.facebook.com/BonJovi",
-            icontwitter: "https://twitter.com/jonbonjovi",
-            iconlinkedin: "https://www.linkedin.com/",
-            iconskype: "https://www.skype.com/uk/",
-            iconemail:"https://www.google.com/intl/uk/gmail/about/eexit"
+        state ={
+            List : [
+                {
+                    id: 1,
+                    name: "Johnny Depp",
+                    description: "Actor",
+                    avatar: 20,
+                    gender: "men",
+                    iconfacebook: "https://ru-ru.facebook.com/JohnChristopherOfficial/",
+                    icontwitter: "https://ru-ru.facebook.com/JohnChristopherOfficial/",
+                    iconlinkedin: "https://www.linkedin.com/",
+                    iconskype: "https://www.skype.com/uk/",
+                    iconemail:"https://www.google.com/intl/uk/gmail/about/",
+                    favorite: false
+                },
+                {
+                    id:2,
+                    name: "Arnold Schwarzenegger",
+                    description: "Actor",
+                    avatar: 35,
+                    gender: "men",
+                    iconfacebook:"https://uk-ua.facebook.com/arnold",
+                    icontwitter: "https://ru-ru.facebook.com/JohnChristopherOfficial/",
+                    iconlinkedin: "https://www.linkedin.com/",
+                    iconskype: "https://www.skype.com/uk/",
+                    iconemail:"https://www.google.com/intl/uk/gmail/about/",
+                    favorite: true
+                },
+                {
+                    id:3,
+                    name: "Sylvester Stallone",
+                    description: "Actor",
+                    avatar: 10,
+                    gender: "men",
+                    iconfacebook:"https://www.facebook.com/SylvesterStallone/",
+                    icontwitter: "https://ru-ru.facebook.com/JohnChristopherOfficial/",
+                    iconlinkedin: "https://www.linkedin.com/",
+                    iconskype: "https://www.skype.com/uk/",
+                    iconemail:"https://www.google.com/intl/uk/gmail/about/",
+                    favorite: false
+                },
+                {
+                    id:4,
+                    name: "Milla Jovovich",
+                    description: "Actor",
+                    avatar: 30,
+                    gender: "women",
+                    iconfacebook:"https://www.facebook.com/MillaJovovich/",
+                    icontwitter: "https://twitter.com/millajovovich",
+                    iconlinkedin: "https://www.linkedin.com/",
+                    iconskype: "https://www.skype.com/uk/",
+                    iconemail:"https://www.google.com/intl/uk/gmail/about/",
+                    favorite: true
+                },
+                {
+                    id:5,
+                    name: "Jon Bon Jovi;",
+                    description: "Singer",
+                    avatar: 50,
+                    gender:"men",
+                    iconfacebook:"https://www.facebook.com/BonJovi",
+                    icontwitter: "https://twitter.com/jonbonjovi",
+                    iconlinkedin: "https://www.linkedin.com/",
+                    iconskype: "https://www.skype.com/uk/",
+                    iconemail:"https://www.google.com/intl/uk/gmail/about/eexit",
+                    favorite: false
+                }
+                ]
         }
-        ];  
-    
+       
+    onDelete = id =>{
+        const index = this.state.List.findIndex(elem => elem.id === id);
+        
+        let newList=[];
+        let counter = 0;
+        
+        
+           for (let i=0; i<this.state.List.length; i++){
+               if (i !==index){
+                  newList[counter]=this.state.List[i];
+                  counter++;
+               }
+        
+        } 
+this.setState(() =>{
+    return{
+            List:newList
+        };
+        });
+        
+       
+    };
+
     render (){
         return(
         <div className="container bootstrap snippet">
             <Search></Search>
-            <ContactList ContactList={this.List} ></ContactList>
+            <ContactList ContactList={this.state.List} onDelete={this.onDelete}></ContactList>
+            
         </div>
     );
 }
